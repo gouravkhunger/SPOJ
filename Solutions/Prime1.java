cb7d5edd7d96f439939bfbb423e8a11cce188f1e
Gourav Khunger <gouravkhunger18@gmail.com>
2020-10-26 08:21:27 +0530

    renamed PRIME1

--- Solutions/Prime1.java
+++ Solutions/Prime1.java
@@ -6,21 +6,21 @@
 import java.util.*;
 import java.lang.*;
 
-class Main
+class PRIME1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int a,b,i,n,j;
 		 Scanner sc = new Scanner(System.in);
-		 
+
 		 n = sc.nextInt();
 		 for(i=0;i<n;i++)
-		 {   
+		 {
 			a = sc.nextInt();
 			b = sc.nextInt();
 		  	System.out.println();
 		  	for(j=a;j<=b;j++)
-		  	{            
+		  	{
 		  		if(checkprime(j))
 		    		System.out.println(j);
 		  	}
@@ -30,7 +30,7 @@ class Main
 	{
 	    if (num <=1)
 	        return false;
-	    else if (num == 2)        
+	    else if (num == 2)
 	        return true;
 	    else if (num % 2 == 0)
 	        return false;
@@ -38,9 +38,9 @@ class Main
 	    {
 	        boolean prime = true;
 	        int divisor = 3;
-	      
+
 	        long upperLimit = (long)Math.sqrt(num) +1;
-	        
+
 	        while (divisor <= upperLimit)
 	        {
 	            if (num % divisor == 0)
